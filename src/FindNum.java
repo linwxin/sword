b2499797e2d1c169c916a795664fcf9292aada08
wanshinlin <wanshinlin@tencent.com>
2020-03-17 11:13:53 +0800

    init

---
+++ src/FindNum.java
@@ -0,0 +1,54 @@
+public class FindNum {
+    public static void main(String []args) {
+        int[][] array = new int[3][4];
+        int num = 1;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                array[i][j] = num++;
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println(Find(5, array))
+    }
+
+    public static boolean Find(int target, int [][] array) {
+        int rowPre = 0;
+        int rowRear = array.length;
+        int colPre = 0;
+        int colRear = array[0].length;
+        int targetRow = 0;
+        while (rowPre < rowRear) {
+            int rowMid = (rowPre + rowRear) / 2;
+            if (target < array[rowMid][0]) {
+                rowRear = rowMid - 1;
+                continue;
+            }
+
+            if (target > array[rowMid][array[0].length - 1]) {
+                rowPre = rowMid + 1;
+                continue;
+            }
+            // 找到所在行
+            if (target >= array[rowMid][0] && target <= array[rowMid][array[0].length - 1]){
+                targetRow = rowMid;
+                while (colPre < colRear) {
+                    int colMid = (colPre + colRear) / 2;
+                    if (target == array[targetRow][colMid])
+                        return true;
+                    else if (target < array[targetRow][colMid])
+                        colRear = colMid - 1;
+                    else
+                        colPre = colMid + 1;
+
+                }
+            }
+
+        }
+        return false;
+    }
+}
