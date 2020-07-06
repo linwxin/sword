74fd32b35b808ffb3ebc7c5d1e702a134227cacb
wanshinlin <wanshinlin@tencent.com>
2020-07-06 11:46:59 +0800

    先去吃饭了

--- src/soword/Solution30.java
+++ src/soword/Solution30.java
@@ -1,5 +1,21 @@
 package soword;
 
 public class Solution30 {
+    public int FindGreatestSumOfSubArray(int[] array) {
+        if (array == null || array.length == 0) return 0;
+        int maxSum = array[0];
+        int curSum = array[0];
 
+        for (int i = 1; i < array.length; i++) {
+            if (curSum + array[i] > array[i]) {
+                curSum = curSum + array[i];
+            } else {
+                curSum = array[i];
+            }
+            if (curSum > maxSum)
+                maxSum = curSum;
+        }
+
+        return maxSum;
+    }
 }
